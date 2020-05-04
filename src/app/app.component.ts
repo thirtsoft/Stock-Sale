7e020caf0585f8de86fd880841cba65f91c0c17d
thir <thirdiallo@gmail.com>
2020-05-04 16:30:10 +0000

    Add datatabe in projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+declare var $;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Stock-Sale';
+
+  @ViewChild('dataTable') table;
+  dataTable: any;
+  constructor(){
+  }
+
+  ngOnInit(): void {
+    this.dataTable = $(this.table.nativeElement);
+    this.dataTable.DataTable();
+  }
+
+
 }
